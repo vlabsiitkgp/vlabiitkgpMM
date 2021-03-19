d2067c7e4321741972de0cafdd55ea3ff6e84461
Sree <tatikondasreelakshmi@gmail.com>
2021-03-19 19:55:03 +0530

    AJM simulator

---
+++ exp3/webpage/assets/js/main.js
@@ -0,0 +1,142 @@
+/*
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+(function ($) {
+
+	var copyRightHTML = '<div class="copyright">&copy; Tatikonda Sree Lakshmi, Research Engineer, VLabs IITKGP.</div>';
+	$("#footer").append(copyRightHTML);
+	
+	$(document).on("click", "#submit_values", function () {
+		$('.air-pump').prop("disabled", false);
+		$('#submit_values').prop("disabled", true);
+	});
+	
+	$(document).on("click", ".air-pump", function () {
+		$('.abrasive-inlet').prop("disabled", false);
+		$('.air-pump').prop("disabled", true);
+		$('#slideshow > div:first')
+					.hide()
+					.next()
+					.show()
+					.end()
+					.appendTo('#slideshow') ;
+	});
+	
+	$(document).on("click", ".abrasive-inlet", function () {
+		$('.start-machining').prop("disabled", false);
+		$('.abrasive-inlet').prop("disabled", true);
+		$('#slideshow > div:first')
+					.hide()
+					.next()
+					.show()
+					.end()
+					.appendTo('#slideshow') ;
+	});
+
+	$(document).on("click", ".start-machining", function () {
+		$('.start-machining').prop("disabled", true);
+		
+		
+		var a_value = parseInt($('#a_value').val());
+		var v_value = parseInt($('#v_value').val());
+		var type = $('#Workpiece').val();
+		var mrr = 0;
+		var H = (Math.random() + 3).toFixed(2);
+		
+		if(type == "brittle"){
+			mrr = ((a_value * Math.pow(v_value,3/2))/ (3^1/4*H^3/4)) * 0.00316;
+		}
+		else{
+			mrr = ((a_value * Math.pow(v_value,2))/H) * (Math.pow(10,-4)/120);
+		}
+		
+		var timesRun = 0;
+		var interval =	setInterval(function () {
+				$('#slideshow > div:first')
+					.hide()
+					.next()
+					.show()
+					.end()
+					.appendTo('#slideshow') ;
+				timesRun += 1;
+				if(timesRun === 22){
+					clearInterval(interval);
+					$('.mt_value').html(mrr.toFixed(2) + " gm/min");
+					$('#submit_values').prop("disabled", false);
+					//$(".mrr_value b").html("(Wb - Wa)/T gm/min");
+					//$(".ewr_value b").html("(Eb - Ea)/T gm/min");
+				}
+			}, 600);
+	
+	});
+
+	function getRandomArbitrary(min, max) {
+	  return Math.random() * (max - min) + min;
+	}
+
+	var $window = $(window),
+		$banner = $('#banner'),
+		$body = $('body');
+
+	// Breakpoints.
+	breakpoints({
+		default: ['1681px', null],
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['361px', '480px'],
+		xxsmall: [null, '360px']
+	});
+
+	// Play initial animations on page load.
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
+	// Menu.
+	$('#menu')
+		.append('<a href="#menu" class="close"></a>')
+		.appendTo($body)
+		.panel({
+			target: $body,
+			visibleClass: 'is-menu-visible',
+			delay: 500,
+			hideOnClick: true,
+			hideOnSwipe: true,
+			resetScroll: true,
+			resetForms: true,
+			side: 'right'
+		});
+
+	$("#slideshow > div:gt(0)").hide();
+	$("#slideshowReverse").hide();
+
+	$('input[type="range"]').on('input', function() {
+
+		var control = $(this),
+		  controlMin = control.attr('min'),
+		  controlMax = control.attr('max'),
+		  controlVal = control.val(),
+		  controlThumbWidth = control.data('thumbwidth');
+
+		var range = controlMax - controlMin;
+
+		var position = ((controlVal - controlMin) / range) * 100;
+		var positionOffset = Math.round(controlThumbWidth * position / 100) - (controlThumbWidth / 2);
+		var output = control.next('output');
+
+		output
+		  .css('left', 'calc(' + position + '% - ' + positionOffset + 'px)')
+		  .text(controlVal);
+
+	});
+	
+	
+
+
+})(jQuery);
---
+++ exp3/webpage/assets/js/main_lc.js
@@ -0,0 +1,137 @@
+/*
+	Industrious by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+(function ($) {
+	$('.error_html').show();
+	var copyRightHTML = '<div class="copyright">&copy; Tatikonda Sree Lakshmi, Research Engineer, VLabs IITKGP.</div>';
+	$("#footer").append(copyRightHTML);
+
+	$(document).on("click", "#submit_values", function () {
+		var lp = $('#laser_power').val();
+		var lss = $('#laser_scan_speed').val();
+		var sod = $('#sod').val();
+		var gp = $('#gas_pressure').val();
+		
+		
+		if(lss == '5000' && sod == '1.75' && gp == '3') {
+			if(lp == '800') {
+				$('.kwt').html('280');
+				$('.kwb').html('200');
+			}
+			else if(lp == '900'){
+				$('.kwt').html('310');
+				$('.kwb').html('215');
+			}
+			else if(lp == '1000'){
+				$('.kwt').html('325');
+				$('.kwb').html('241');
+			}
+			else{
+				errorHTML();
+			}
+		}
+		else if(lp == '900' && sod == '1.75' && gp == '3') {
+			if(lss == '3000') {
+				$('.kwt').html('410');
+				$('.kwb').html('210');
+			}
+			else if(lss == '4000'){
+				$('.kwt').html('335');
+				$('.kwb').html('235');
+			}
+			else if(lss == '5000'){
+				$('.kwt').html('310');
+				$('.kwb').html('215');
+			}
+			else{
+				errorHTML();
+			}
+		}
+		else if(lp == '900' && lss == '4000' && gp == '3') {
+			if(sod == '1') {
+				$('.kwt').html('395');
+				$('.kwb').html('300');
+			}
+			else if(sod == '1.75'){
+				$('.kwt').html('335');
+				$('.kwb').html('235');
+			}
+			else if(sod == '2.5'){
+				$('.kwt').html('385');
+				$('.kwb').html('280');
+			}
+			else{
+				errorHTML();
+			}
+		}
+		else if(lp == '900' && lss == '4000' && sod == '1.75') {
+			if(gp == '3') {
+				$('.kwt').html('335');
+				$('.kwb').html('235');
+			}
+			else if(gp == '4'){
+				$('.kwt').html('375');
+				$('.kwb').html('305');
+			}
+			else if(gp == '5'){
+				$('.kwt').html('355');
+				$('.kwb').html('310');
+			}
+			else{
+				errorHTML();
+			}
+		}
+		else{
+			errorHTML();
+		}
+	});
+
+	function errorHTML(){
+		$('.kwt').html('');
+		$('.kwb').html('');
+		$('.error_html').css('color', 'red');
+        $('.error_html').html("Invalid Input");
+        $('.error_html').show();
+        $('.error_html').delay(2000).fadeOut('slow');
+	}
+	
+	var $window = $(window),
+		$banner = $('#banner'),
+		$body = $('body');
+
+	// Breakpoints.
+	breakpoints({
+		default: ['1681px', null],
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['361px', '480px'],
+		xxsmall: [null, '360px']
+	});
+
+	// Play initial animations on page load.
+	$window.on('load', function () {
+		window.setTimeout(function () {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
+
+	// Menu.
+	$('#menu')
+		.append('<a href="#menu" class="close"></a>')
+		.appendTo($body)
+		.panel({
+			target: $body,
+			visibleClass: 'is-menu-visible',
+			delay: 500,
+			hideOnClick: true,
+			hideOnSwipe: true,
+			resetScroll: true,
+			resetForms: true,
+			side: 'right'
+		});
+
+})(jQuery);
