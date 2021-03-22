e41cb2b698060406a152d055a7aed33bcee3bf01
Sree <tatikondasreelakshmi@gmail.com>
2021-03-22 12:16:54 +0530

    22/6

--- exp3/webpage/assets/js/main.js
+++ exp3/webpage/assets/js/main.js
@@ -42,7 +42,7 @@
 		var H = (Math.random() + 3).toFixed(2);
 
 		if(type == "brittle"){
-			mrr = ((a_value * Math.pow(v_value,3/2))/ (3^1/4*H^3/4)) * 0.000316;
+			mrr = ((a_value * Math.pow(v_value,3/2))/ (3^1/4*H^3/4)) * 0.00316;
 		}
 		else{
 			mrr = ((a_value * Math.pow(v_value,2))/H) * (Math.pow(10,-2)/120);
